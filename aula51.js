696bc7a34c99103b8238b8cee1cb51cf90671f58
Pedro <pedrohenrique_3333@outlook.com>
2023-10-10 21:15:21 -0300

    aula-51

---
+++ aula51.js
@@ -0,0 +1,23 @@
+//return
+//retorna um valor
+//termina a funcao
+
+function soma(a, b) {
+  return a + b;
+}
+
+function criaPessoa(nome, sobrenome) {
+  return {
+    nome,
+    sobrenome,
+  };
+}
+const p1 = criaPessoa("luiz", "Otavio");
+
+const p2 = {
+  nome: "Joao",
+  sobrenome: "Oliveira",
+};
+//os 2 criam o mesmo objeto
+
+const olaMundo = falaFrase("Ola");
